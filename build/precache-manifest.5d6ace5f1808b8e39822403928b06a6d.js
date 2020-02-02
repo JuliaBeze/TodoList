88a1154d79766544c900cb948b5a2dc7dc0359ee
JuliaBeze <55660254+JuliaBeze@users.noreply.github.com>
2020-02-02 14:14:05 +0300

    Add files via upload

---
+++ build/precache-manifest.5d6ace5f1808b8e39822403928b06a6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8427a1f5c10d61f152099db05a80fc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d35cb0101124f1058f21",
+    "url": "/static/css/main.759cb2d0.chunk.css"
+  },
+  {
+    "revision": "1d3f4781706680d0b375",
+    "url": "/static/js/2.7643b099.chunk.js"
+  },
+  {
+    "revision": "d35cb0101124f1058f21",
+    "url": "/static/js/main.96983416.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
